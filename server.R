d543a0d2b67d37052096739eab31c1589fafbcab
CICU Mihai CONSULTANT GBA <Mihai.Cicu@orange.com>
2018-04-23 17:32:50 +0300

    Added mtcars table view

--- server.R
+++ server.R
@@ -1,26 +1,43 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+library(DT)
+
+#helper method
+shinyInput = function(FUN, len, id, ...) {
+  inputs = character(len)
+  for (i in seq_len(len)) {
+    inputs[i] = as.character(FUN(paste0(id, i), label = NULL, ...))
+  }
+  inputs
+}
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
+  output$cars = DT::renderDataTable({
+    data.frame(mtcars,
+               Rating = shinyInput(
+                 selectInput,
+                 nrow(mtcars),
+                 "selecter_",
+                 choices = 1:5,
+                 width = "60px"
+               ))
+  }, selection = 'none', server = FALSE, escape = FALSE, options = list(
+    # paging = TRUE,
+    # preDrawCallback = JS(
+    #   'function() {
+    #   Shiny.unbindAll(this.api().table().node()); }'
+    # ),
+    # drawCallback = JS('function() {
+    #                   Shiny.bindAll(this.api().table().node()); } ')
+     ))
   
 })
