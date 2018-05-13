bae47f183c2562b0c02ea2b307f0ad529c6a216f
Cicu Mihai <cicumihai02@gmail.com>
2018-05-13 13:39:20 +0300

    working linked dropdowns shiny only

--- server.R
+++ server.R
@@ -11,57 +11,77 @@ library(shiny)
 library(DT)
 
 #helper method
-shinyInput = function(FUN, len, id, ...) {
+shinyInput = function(FUN, len, id, dropdownValues, ...) {
   inputs = character(len)
   for (i in seq_len(len)) {
-    inputs[i] = as.character(FUN(paste0(id, i), label = NULL, ...))
+    inputs[i] = as.character(FUN(paste0(id, i), label = NULL, choices = dropdownValues, ...))
   }
   inputs
 }
 
-datacars = mtcars;
-datacars["rating"] = 1;
+datacars = mtcars
+
+datacars["producator"] = "Other"
+
+datacars["ratingProducator"] = "*"
+
+
+listaProducatori = list(
+  producatori = c("Mercedes" = 1, "BMW" = 2, "Porsche"= 3, "Mazda" = 4, "Other" = 5),
+  ratingProducatori = c("*****" = 1, "****" = 2, "***" = 3, "**" = 4, "*" = 5)
+)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  
+shinyServer(function(input, output, session) {
   output$cars = DT::renderDataTable({
     data.frame(
       mtcars,
-      Rating = shinyInput(
+      producator = shinyInput(
         selectInput,
         nrow(mtcars),
-        "selecter",
-        choices = 1:5,
-        width = "60px"
+        "producator",
+        dropdownValues = listaProducatori$producatori,
+        width = "100px"
       ),
-      test = "1"
+      ratingProducator = shinyInput(
+        selectInput,
+        nrow(mtcars),
+        "ratingProducator",
+        dropdownValues = listaProducatori$ratingProducatori,
+        width = "50px"
+      )
     )
   }, selection = 'none', server = FALSE, escape = FALSE, options = list(
     paging = TRUE,
-    preDrawCallback = JS( #important pt dropdown reactiv
+    preDrawCallback = JS(
+      #important pt dropdown reactiv
       'function() {
       console.log("preDrawCallback");
       Shiny.unbindAll(this.api().table().node()); }'
     ),
-    drawCallback = JS( #important pt dropdown reactiv
+    drawCallback = JS(
+      #important pt dropdown reactiv
       'function() {
       console.log("DrawCallback");
       Shiny.bindAll(this.api().table().node()); } '
     )
-  ))
+    ))
   
   
   observe({
-    for (dropdownid in grep("selecter", names(input), value = TRUE)) {
+    for (dropdownid in grep("producator", names(input), value = TRUE)) {
       #extract row index from dropdownid (e.g. "selecter2" => 2)
-      rowindex = as.integer(substr(dropdownid, nchar("selecter")+1, nchar(dropdownid)))
+      rowindex = as.integer(substr(dropdownid, nchar("producator") + 1, nchar(dropdownid)))
       
-      datacars$rating[rowindex] = input[[dropdownid]]
+      dropdownindex = as.integer(input[[dropdownid]]);
+      datacars$producator[rowindex] = names(listaProducatori$producatori)[dropdownindex]
+      datacars$ratingProducator[rowindex] = names(listaProducatori$ratingProducatori)[dropdownindex]
+      
+      updateSelectInput(session, paste0("ratingProducator", rowindex), choices = listaProducatori$ratingProducatori, selected = dropdownindex)
     }
-    print(head(datacars))
+    
+    print(datacars)
   })
   
   
   })
-
